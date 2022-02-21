26252c8a5290731d5656d2100a79bc4fc22af9de
kulakberkay15@gmail.com <kulakberkay15@gmail.com>
2022-02-21 13:50:48 +0300

    [NEW] Manipulating DOM With JavaScript

--- 1) Quote Generator/script.js
+++ 1) Quote Generator/script.js
@@ -1,8 +1,50 @@
+const quoteContainer = document.getElementById("quote-container");
+const quoteText = document.getElementById("quote");
+const authorText = document.getElementById("author");
+const twitterBtn = document.getElementById("twitter");
+const newQuoteButn = document.getElementById("new-quote");
+
+let apiQuotes = [];
+
 // Show New Quote
 function newQuote() {
   // Pick a random quote from apiQuotes array
-  const quote = localQuotes[Math.floor(Math.random() * localQuotes.length)];
-  console.log(quote);
+  const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+  // Check if Author field is blank and replace it with 'Unknown'
+  if (!quote.author) {
+    authorText.textContent = "Unknown";
+  } else {
+    authorText.textContent = quote.author;
+  }
+
+  // Check Quote length to determine styling
+  if (quote.text.length > 50) {
+    quoteText.classList.add("long-quote");
+  } else {
+    quoteText.classList.remove("long-quote");
+  }
+
+  quoteText.textContent = quote.text;
 }
 
-newQuote();
+// Get Quotes Form API
+async function getQuotes() {
+  const apiUrl = "https://type.fit/api/quotes";
+  try {
+    const response = await fetch(apiUrl);
+    apiQuotes = await response.json();
+    newQuote();
+  } catch (error) {}
+}
+
+// Tweeet Quote
+function tweetQuote() {
+  const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+  window.open(twitterUrl, "_blank");
+}
+
+// Event Listeners
+newQuoteButn.addEventListener("click", newQuote);
+twitterBtn.addEventListener("click", tweetQuote);
+// On load
+getQuotes();
