cb787cfc4e2f64d6dae2c07e6d13268f75137e8a
kulakberkay15@gmail.com <kulakberkay15@gmail.com>
2022-02-23 15:06:19 +0300

    Handling Click Events

--- 4) Guess My Number/script.js
+++ 4) Guess My Number/script.js
@@ -1,10 +1,23 @@
 'use strict';
 
-console.log(document.querySelector('.message').textContent);
+// console.log(document.querySelector('.message').textContent);
 
-document.querySelector('.message').textContent = '✌️ Correct Number!';
+// document.querySelector('.message').textContent = '✌️ Correct Number!';
 
-document.querySelector('.number').textContent = 13;
-document.querySelector('.score').textContent = 10;
-console.log(document.querySelector('.guess').value);
-document.querySelector('.guess').value = 23;
+// document.querySelector('.number').textContent = 13;
+// document.querySelector('.score').textContent = 10;
+// console.log(document.querySelector('.guess').value);
+// document.querySelector('.guess').value = 23;
+
+const x = function () {
+  console.log(23);
+};
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = 'No Number 💥';
+  }
+});
