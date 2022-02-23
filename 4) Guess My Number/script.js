6ff7756c3a309364b1cd880ee610ec21113dc80b
kulakberkay15@gmail.com <kulakberkay15@gmail.com>
2022-02-23 16:00:11 +0300

    Manipulating CSS Styles

--- 4) Guess My Number/script.js
+++ 4) Guess My Number/script.js
@@ -17,10 +17,17 @@ document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess, typeof guess);
 
+  // When there is no input
   if (!guess) {
     document.querySelector('.message').textContent = 'No Number 💥';
+    // when player wins
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = '✌️ Correct Number!';
+    document.querySelector('body').style.backgroundColor = '#60b347';
+
+    document.querySelector('.number').style.width = '30rem';
+
+    // quess is to high
   } else if (guess > secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = 'Too high';
@@ -30,6 +37,7 @@ document.querySelector('.check').addEventListener('click', function () {
       document.querySelector('.message').textContent = 'You Lost the game!';
       document.querySelector('.score').textContent = 0;
     }
+    // quess is to low
   } else if (guess < secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = 'Too Low';
