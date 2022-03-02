f71d54a7c3eaaa3b9ef2da848a4da5db1c32e5d1
kulakberkay15@gmail.com <kulakberkay15@gmail.com>
2022-03-02 16:04:13 +0300

    JS - Progress Bar (Seek)

--- 12) Video Player/script.js
+++ 12) Video Player/script.js
@@ -32,6 +32,27 @@ video.addEventListener("ended", showPlayIcon);
 
 // Progress Bar ---------------------------------- //
 
+function displayTime(time) {
+  const minutes = Math.floor(time / 60);
+  let seconds = Math.floor(time % 60);
+  seconds = seconds > 9 ? seconds : `0${seconds}`;
+  return `${minutes}:${seconds}`;
+}
+
+// Update progress bar as video plays
+function updateProgress() {
+  progressBar.style.width = `${(video.currentTime / video.duration) * 100}%`;
+  currentTime.textContent = `${displayTime(video.currentTime)} /`;
+  duration.textContent = `${displayTime(video.duration)}`;
+}
+
+// Click to seek within the video
+function setProgress(e) {
+  const newTime = e.offsetX / progressRange.offsetWidth;
+  progressBar.style.width = `${newTime * 100}%`;
+  video.currentTime = newTime * video.duration;
+}
+
 // Volume Controls --------------------------- //
 
 // Change Playback Speed -------------------- //
@@ -40,3 +61,6 @@ video.addEventListener("ended", showPlayIcon);
 
 playBtn.addEventListener("click", togglePlay);
 video.addEventListener("click", togglePlay);
+video.addEventListener("timeupdate", updateProgress);
+video.addEventListener("canplay", updateProgress);
+progressRange.addEventListener("click", setProgress);
