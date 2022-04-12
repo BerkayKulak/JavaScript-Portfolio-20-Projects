0f7bd2a0b6a2271fea88b27d09209fb19c0ea5d7
kulakberkay15@gmail.com <kulakberkay15@gmail.com>
2022-04-12 20:54:02 +0300

    Local Storage ile Çalışma

--- 28) Todo App/script.js
+++ 28) Todo App/script.js
@@ -1,80 +1,121 @@
-// variables
+// UI vars
 
 const form = document.querySelector("form");
 const input = document.querySelector("#txtTaskName");
 const btnDeleteAll = document.querySelector("#btnDeleteAll");
 const taskList = document.querySelector("#task-list");
-const items = ["item 1", "item 2", "item 3", "item 4"];
+let items;
 
 // load items
 loadItems();
 
+// call event listeners
 eventListeners();
 
 function eventListeners() {
+  // submit event
   form.addEventListener("submit", addNewItem);
 
+  // delete an item
   taskList.addEventListener("click", deleteItem);
 
+  // delete all items
   btnDeleteAll.addEventListener("click", deleteAllItems);
 }
 
 function loadItems() {
+  items = getItemsFromLS();
   items.forEach(function (item) {
     createItem(item);
   });
 }
 
+// get items from Local Storage
+function getItemsFromLS() {
+  if (localStorage.getItem("items") === null) {
+    items = [];
+  } else {
+    items = JSON.parse(localStorage.getItem("items"));
+  }
+  return items;
+}
+
+// set item to Local Storage
+function setItemToLS(text) {
+  items = getItemsFromLS();
+  items.push(text);
+  localStorage.setItem("items", JSON.stringify(items));
+}
+
+// delete item from LS
+function deleteItemFromLS(text) {
+  items = getItemsFromLS();
+  items.forEach(function (item, index) {
+    if (item === text) {
+      items.splice(index, 1);
+    }
+  });
+  localStorage.setItem("items", JSON.stringify(items));
+}
+
 function createItem(text) {
+  // create li
   const li = document.createElement("li");
   li.className = "list-group-item list-group-item-secondary";
   li.appendChild(document.createTextNode(text));
 
+  // create a
   const a = document.createElement("a");
   a.classList = "delete-item float-right";
   a.setAttribute("href", "#");
   a.innerHTML = '<i class="fas fa-times"></i>';
 
+  // add a to li
   li.appendChild(a);
 
+  // add li to ul
   taskList.appendChild(li);
 }
 
+// add new item
 function addNewItem(e) {
   if (input.value === "") {
-    alert("add a task");
+    alert("add new item");
   }
 
+  // create item
   createItem(input.value);
 
+  // save to LS
+  setItemToLS(input.value);
+
+  // clear input
   input.value = "";
 
   e.preventDefault();
 }
 
+// delete an item
 function deleteItem(e) {
   if (e.target.className === "fas fa-times") {
-    e.target.parentElement.parentElement.remove();
-  }
+    if (confirm("are you sure ?")) {
+      e.target.parentElement.parentElement.remove();
 
+      // delete item from LS
+      deleteItemFromLS(e.target.parentElement.parentElement.textContent);
+    }
+  }
   e.preventDefault();
 }
 
+// delete all items
 function deleteAllItems(e) {
-  //
-  //taskList.innerHTML = "";
-
-  // while (taskList.firstChild) {
-  //     taskList.removeChild(taskList.firstChild);
-  // }
-
-  if (confirm("are you sure")) {
-    taskList.childNodes.forEach(function (item) {
-      if (item.nodeType === 1) {
-        item.remove();
-      }
-    });
+  if (confirm("are you sure ?")) {
+    // taskList.innerHTML='';
+    while (taskList.firstChild) {
+      taskList.removeChild(taskList.firstChild);
+    }
+    localStorage.clear();
   }
-
   e.preventDefault();
 }
